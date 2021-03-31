6720d23658a2a1bc4e22c19d3dba6d272871d365
alex-pancho <pan.pan.4it@gmail.com>
2021-03-31 10:26:03 +0300

    no message

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,4 +3,16 @@ from calc import calc_me
 
 
 def test_add():
-    assert 5 == calc_me(3,2,"+")
+    assert calc_me(3,2,"+") == 5
+
+def test_devide_by_zero_wrong():
+    assert calc_me(10,0,"/") == "ERROR: Divide by zero!"
+
+
+def test_devide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        calc_me(10,0,"/")
+
+def test_devide_by_zero_with_msg():
+    with pytest.raises(ZeroDivisionError, match="ERROR: Divide by zero!"):
+        calc_me(10,0,"/")
