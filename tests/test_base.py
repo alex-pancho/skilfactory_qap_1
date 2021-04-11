b1f6a847a2f1000966b6e98d743b777d7bd01b61
alex-pancho <pan.pan.4it@gmail.com>
2021-04-11 14:07:36 +0300

    new test for dev

--- tests/test_base.py
+++ tests/test_base.py
@@ -18,10 +18,32 @@ def test_div():
     assert calc_me(111111111, 9, "/") == 12345679
 
 
+def test_pow():
+    assert calc_me(4, 2, "^") == 16
+
+
+def test_first_str():
+    assert calc_me("a", 9, "/") == "ERROR: now it is does not supported"
+
+
+def test_second_str():
+    assert calc_me(2, "b", "+") == "ERROR: now it is does not supported"
+
+
+def test_first_none():
+    with pytest.raises(ValueError, match="ERROR: send me Number1"):
+        calc_me(None, 9, "/")
+
+
+def test_second_none():
+    with pytest.raises(ValueError, match="ERROR: send me Number2"):
+        calc_me(2, None, "+")
+
+
 def test_division_by_zero_with_msg():
     with pytest.raises(ZeroDivisionError, match="ERROR: division by zero!"):
         calc_me(10,0,"/")
 
 def test_unknown_operation():
     with pytest.raises(ValueError, match="ERROR: Unknown operation"):
-        calc_me(10,5,"^")
+        calc_me(10,5,"@")
