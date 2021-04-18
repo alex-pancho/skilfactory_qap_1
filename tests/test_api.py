694e09772ff6a60f414764688ebb137c18f22654
alex-pancho <pan.pan.4it@gmail.com>
2021-04-18 22:46:46 +0300

    fix for fixture

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,9 +2,8 @@ import apihelper
 import pytest
 
 
-def test_get_key_positive():
-    user = apihelper.user
-    result = apihelper.get_apikey(user["email"], user["password"])
+def test_get_key_positive(testuser):
+    result = apihelper.get_apikey(testuser["email"], testuser["password"])
     assert result.get("key") is not None
 
 @pytest.mark.usefixtures("authenticate")
