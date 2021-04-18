9697250ed97ebe554307f60250cdd76257d86142
alex-pancho <pan.pan.4it@gmail.com>
2021-04-18 22:43:07 +0300

    new tests for api

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,5 +5,21 @@ import pytest
 def test_get_key_positive():
     user = apihelper.user
     result = apihelper.get_apikey(user["email"], user["password"])
-    print(result.get("key"))
     assert result.get("key") is not None
+
+@pytest.mark.usefixtures("authenticate")
+def test_get_pets():
+    result = apihelper.get_pets()
+    assert result.get('pets') is not None, result
+
+@pytest.mark.usefixtures("authenticate")
+def test_post_pets():
+    name = "Helen"
+    animal_type = "ladybug"
+    age = 12
+    path_for_picture = "D:\\OneDrive\\Current_job\\api_qa_git\\testdata\\qa_180x180.jpg"
+    result = apihelper.post_pets(name, animal_type, age, path_for_picture)
+    assert result.get('name') == name
+    assert result.get('animal_type') == animal_type
+    assert result.get('age') == str(age)
+    assert "data:image/jpeg;base64" in result.get('pet_photo')
